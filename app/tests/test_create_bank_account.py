1ab24fa2e129e845ae61130662774d0b99914cfb
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2022-11-25 14:40:06 +0100

    testy kredytu

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -277,3 +277,79 @@ class TestFunkcjonalnoscKonta(unittest.TestCase):
         # func
         konto.wyplac_express(10)
         self.assertEqual(konto.saldo, 85, "Złe działanioe funkcji!")
+
+
+class TestHistorii(unittest.TestCase):
+    def test_konto_firmowe_historia(self):
+        # initial
+        konto = KontoFirmowe("Miotek Company", "0123456789")
+        self.assertEqual(len(konto.nip), 10, "Niepoprawny NIP!")
+        konto.saldo = 10000
+        konto.wyplac(100)
+        konto.wyplac_express(50)
+        konto.wplac(30)
+
+        # func
+        self.assertEqual(konto.historia, [-100, -50, -5, 30], "Niepoprawnie zaksięgoiwana historia")
+
+
+    def test_konto_prywatne_historia(self):
+        # initial
+        konto = KontoPrywatne("Daniel", "Faltynowski", "02261734567")
+        self.assertEqual([konto.imie, konto.nazwisko, len(konto.pesel)], ["Daniel", "Faltynowski", 11],
+                         "Niepoprawne dane konta!")
+        konto.saldo = 10000
+        konto.wyplac(100)
+        konto.wyplac_express(50)
+        konto.wplac(30)
+
+        # func
+        self.assertEqual(konto.historia, [-100, -50, -1, 30], "Niepoprawnie zaksięgoiwana historia")
+
+
+class test_zaciaganie_kredytu(unittest.TestCase):
+
+    imie = "Daniel"
+    nazwisko = "Faltynowski"
+    pesel = "0226174567"
+
+    def test_kredyt_prywatne_konto_brak(self):
+        konto = KontoPrywatne(self.imie, self.nazwisko, self.pesel)
+        self.assertEqual(konto.zaciagnij_kredyt(1000), False, "Nie spełniono warunków do zaciągnięcia kredytu")
+
+
+    def test_kredyt_prywatne_konto_nie_spelnione(self):
+        konto = KontoPrywatne(self.imie, self.nazwisko, self.pesel)
+        konto.wplac(100000)
+        konto.wyplac(100)
+        konto.wyplac(3000)
+        konto.wplac(1000)
+        konto.wyplac(100)
+        konto.wplac(10)
+        self.assertEqual(konto.zaciagnij_kredyt(1000), False, "Nie spełniono warunków do zaciągnięcia kredytu")
+
+
+    def test_kredyt_prywatne_konto_spelnione(self):
+        konto = KontoPrywatne(self.imie, self.nazwisko, self.pesel)
+        konto.wplac(100000)
+        konto.wyplac(100)
+        konto.wyplac(3000)
+        konto.wplac(1000)
+        konto.wyplac(100)
+        konto.wplac(10)
+        konto.wplac(10000)
+        konto.wplac(1000)
+        self.assertEqual(konto.zaciagnij_kredyt(1000), True, "Nie spełniono warunków do zaciągnięcia kredytu")
+
+
+    def test_kredyt_prywatne_konto_niespelniony_jeden_warunek(self):
+        konto = KontoPrywatne(self.imie, self.nazwisko, self.pesel)
+        konto.wplac(100000)
+        konto.wyplac(100)
+        konto.wyplac(3000)
+        konto.wplac(1000)
+        konto.wyplac(100)
+        konto.wplac(10)
+        konto.wplac(10)
+        konto.wplac(10)
+        self.assertEqual(konto.zaciagnij_kredyt(1000), False, "Nie spełniono warunków do zaciągnięcia kredytu")
